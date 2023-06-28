7854e363910c15668d4a4c01a12efd6f07232a7b
Ivan <ivanchul@yandex.ru>
2023-06-28 17:18:53 +0400

    Addded diagram

--- Control_Work/Program.cs
+++ Control_Work/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = new string[5];
+array[0] = "Sad";
+Console.WriteLine(array.Length);
+if (array[0].Length <= 3)
+{
+    Console.WriteLine(array[0]);
+}
+Array.Resize(ref array, array.Length+1);
+Console.WriteLine(array.Length);
